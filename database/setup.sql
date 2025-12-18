23916cd6c9bf079a9cf92942f5a7d9a52be56b9b
N-Dave-08 <cdave.dev@gmail.com>
2025-12-18 14:42:02 +0800

    Add initial database setup and user data

---
+++ database/setup.sql
@@ -0,0 +1,14 @@
+CREATE TABLE users (
+  id INTEGER,
+  name TEXT,
+  age INTEGER,
+  country TEXT
+);
+
+INSERT INTO users VALUES
+(1, 'Ana', 22, 'PH'),
+(2, 'Ben', 30, 'US'),
+(3, 'Cara', 19, 'PH'),
+(4, 'Dan', 25, 'PH'),
+(5, 'Eva', 35, 'UK');
+
